404914d322fae980ce955234aeceaa4b3f4e324e
Conor Lorsung <cglorsung@gmail.com>
2018-04-03 20:17:24 -0500

    A little farther.

--- GeneTesting.R
+++ GeneTesting.R
@@ -23,7 +23,13 @@ sigmoid <- function(x, derive=FALSE) {
 if(supervise) {
   outArr <- array(c(1,1,1,1,0,0,0,0,0,1,0,1), dim=c(12,1,1))
 } else {
-  
+  outArr <- array(c(0), dim=c(nrow(scaled.fileData), 1, 1))
 }
 
 print(outArr)
+
+print(ncol(scaled.fileData))
+
+synapse0 <- 2*runif(ncol(scaled.fileData), -1.0, 1.0) - 1
+
+print(synapse0)
