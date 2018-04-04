c622d40430b082cc12a952916a373a1ded921999
Conor Lorsung <cglorsung@gmail.com>
2018-04-03 19:12:47 -0500

    Initial push

---
+++ GeneTesting.R
@@ -0,0 +1,29 @@
+
+# Supervise the learning?
+supervise = TRUE
+
+# Number of iterations
+iterations = 10000
+
+# Read data file
+fileData <- read.csv("Data/SampleGenes.csv", header=FALSE)
+
+scaled.fileData <- scale(fileData)
+print(scaled.fileData[1,1:10])
+
+# Sigmoid function
+sigmoid <- function(x, derive=FALSE) {
+  if (derive) {
+    return (x*(1-x))
+  } else {
+    return (1/(1+exp(-x)))
+  }
+}
+
+if(supervise) {
+  outArr <- array(c(1,1,1,1,0,0,0,0,0,1,0,1), dim=c(12,1,1))
+} else {
+  
+}
+
+print(outArr)
